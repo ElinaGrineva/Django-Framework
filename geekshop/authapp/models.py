457314aa205db2b17ce5c5eb1f9b064885693d89
Elina Grineva <elina.grineva@yandex.ru>
2021-11-18 15:42:36 +0300

    added a 1hw - Optimization

---
+++ geekshop/authapp/models.py
@@ -0,0 +1,25 @@
+import pytz
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from datetime import datetime, timedelta
+from django.conf import settings
+
+
+class ShopUser(AbstractUser):
+    avatar = models.ImageField(upload_to='users_avatars', blank=True, verbose_name='Avatar')
+    age = models.PositiveSmallIntegerField(verbose_name='Age')
+
+    activate_key = models.CharField(max_length=128, verbose_name='Ключ активации', blank=True, null=True)
+    activate_key_expired = models.DateTimeField(blank=True, null=True)
+
+    def is_activate_key_expired(self):
+        if datetime.now(pytz.timezone(settings.TIME_ZONE)) > self.activate_key_expired + timedelta(hours=48):
+            return True
+        else:
+            return False
+
+    def activate_user(self):
+        self.is_active = True
+        self.activate_key = None
+        self.activate_key_expired = None
+        self.save()
